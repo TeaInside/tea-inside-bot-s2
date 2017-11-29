716f3f349f7cf6cfd629a2e51cf62a313e9902bb
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-29 19:33:56 +0700

    Fix .gitignore

--- connector/telegram/debug.php
+++ connector/telegram/debug.php
@@ -1,4 +1,4 @@
 <?php
 
-file_put_contents(LOG_DIR."/telegram/input.last", json_encode(json_decode($input), 128));
+file_put_contents(TELEGRAM_LOG_DIR."/input.last", json_encode(json_decode($input), 128));
 die(0);
