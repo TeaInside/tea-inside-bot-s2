266a041d678cd97cd3326f688df4bac9fb93292b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-20 17:42:30 +0700

    Improve group handler

--- src/Bot/Telegram/Models/Group.php
+++ src/Bot/Telegram/Models/Group.php
@@ -41,11 +41,13 @@ class Group
 
 	public static function insertAdmins($admins, $group_id)
 	{
+		$st = DB::prepare($quer = "INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `display_name`, `photo`, `authority`, `is_bot`, `created_at`, `updated_at`) VALUES (:user_id, :username, :first_name, :last_name, :display_name, :photo, :authority, :is_bot, :created_at, :updated_at);");
+		$stq = DB::prepare("INSERT INTO `user_history` (`user_id`, `username`, `first_name`, `last_name`, `display_name`, `photo`, `created_at`) VALUES  (:user_id, :username, :first_name, :last_name, :display_name, :photo, :created_at);");
+		$std = DB::prepare("INSERT INTO `group_admins` (`user_id`, `group_id`, `status`, `can_be_edited`, `can_change_info`, `can_delete_messages`, `can_invite_users`, `can_restrict_members`, `can_pin_messages`, `can_promote_members`, `created_at`) VALUES (:user_id, :group_id, :status, :can_be_edited, :can_change_info, :can_delete_messages, :can_invite_users, :can_restrict_members, :can_pin_messages, :can_promote_members, :created_at);");
 		foreach ($admins as $key => $data) {
 			$fx = function ($key, $default = null) use ($data) {
 				return isset($data['user'][$key]) ? $data['user'][$key] : $default;
 			};
-			$st = DB::prepare("INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `display_name`, `photo`, `authority`, `is_bot`, `created_at`, `updated_at`) VALUES (:user_id, :username, :first_name, :last_name, :display_name, :photo, :authority, :is_bot, :created_at, :updated_at);");
 			if ($st->execute(
 				$dataq = [
 					":user_id"  => $fx('user_id'),
@@ -55,19 +57,32 @@ class Group
 					":display_name"=> $fx('display_name'),
 					":photo"=> $fx('photo'),
 					":authority"=> $fx('authority', 'user'),
-					":is_bot"=>$fx('is_bot', 0),
+					":is_bot"=>(int)$fx('is_bot', 0),
 					":created_at"=> $fx('created_at', date("Y-m-d H:i:s")),
 					":updated_at" => null
 				]
 			)) {
-				$st = DB::prepare("INSERT INTO `user_history` (`user_id`, `username`, `first_name`, `last_name`, `display_name`, `photo`, `created_at`) VALUES  (:user_id, :username, :first_name, :last_name, :display_name, :photo, :created_at);");
 				unset($dataq[':authority'], $dataq[':is_bot'], $dataq[':updated_at']);
-				pc($st->execute($dataq), $st);
+				pc($stq->execute($dataq), $stq);
 			}
-			var_dump($data);
-			foreach ($data as $key => $val) {
-				$st  = DB::prepare("INSERT INTO `group_admins` (`user_id`, `group_id`, `status`, `can_be_edited`, `can_change_info`, `can_delete_messages`, `can_invite_users`, `can_restrict_members`, `can_pin_messages`, `can_promote_members`, `created_at`)");
-			}
+			$fx = function ($key, $default = null) use ($data) {
+				return isset($data[$key]) ? $data[$key] : $default;
+			};
+			pc($std->execute(
+				[
+					":user_id" => $data['user']['id'],
+					":group_id"	=> $group_id,
+					":status"	=> $data['status'],
+					":can_be_edited" => (int) $fx('can_be_edited',1),
+					":can_change_info" => (int) $fx("can_change_info",1),
+					":can_delete_messages" => (int) $fx("can_delete_messages",1),
+					":can_invite_users"	=> (int) $fx("can_invite_users", 1),
+					":can_restrict_members"	=> (int) $fx("can_restrict_members", 1),
+					":can_pin_messages" => (int) $fx("can_pin_messages", 1),
+					":can_promote_members"	=> (int) $fx("can_promote_members"),
+					":created_at"	=> $fx("created_at", date("Y-m-d H:i:s"))
+				]
+			), $std);
 		}
 	} 
 }
