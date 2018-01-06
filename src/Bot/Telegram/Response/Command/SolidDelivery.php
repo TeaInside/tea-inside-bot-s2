f88cc886e47b676c17a236f4a803dbbde853f28d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-06 12:39:18 +0700

    Improve forwarder

--- src/Bot/Telegram/Response/Command/SolidDelivery.php
+++ src/Bot/Telegram/Response/Command/SolidDelivery.php
@@ -18,23 +18,41 @@ class SolidDelivery extends CommandAbstraction implements EventContract
     public function run()
     {
         if ($this->e['msg_type'] === "text") {
-            LINE::bg()::push(
-                [
-                    "to" => "Ce20228a1f1f98e6cf9d6f6338603e962",
-                    "messages" => LINE::buildLongTextMessage("{$this->e['name']}\n\n{$this->e['text']}")
-                ]
-            );
+            $data = [
+                "to" => "Ce20228a1f1f98e6cf9d6f6338603e962",
+                "messages" => LINE::buildLongTextMessage("{$this->e['name']}\n\n{$this->e['text']}")
+            ];
+        } elseif ($this->e['msg_type'] === "photo") {
+            $data[] = [
+                "type" => "image",
+                "originalContentUrl" => $url,
+                "previewImageUrl" => $url
+            ];
+            if (! empty($this->e['text'])) {
+                $data[] = [
+                    "type" => "text",
+                    "text" => "{$this->e['name']}\n\n{$this->e['text']}"
+                ];
+            }
+            $data = [
+                "to" => "Ce20228a1f1f98e6cf9d6f6338603e962",
+                "messages" => $data
+            ];
         }
+        LINE::bg()::push($data);
     }
 
     private function savePhoto()
     {
-        $p = end($this->b->photo);
+        $p = end($this->e['photo']);
         $a = json_decode(B::getFile([
             "file_id" => $p['file_id']
         ])['content'], true);
         $st = new Curl("https://api.telegram.org/file/bot".TOKEN."/".$a['result']['file_path']);
-        file_put_contents(data."/line/tmp/".($t = time()).".jpg", $st->exec());
-        return "https://webhook.crayner.cf/storage/data/line/tmp/".$t.".jpg";
+        if (file_put_contents(data."/tmp/".($t = sha1($p['file_id'])).".jpg", $st->exec())) {
+            return STORAGE_URL."/tmp/{$t}.jpg";
+        } else {
+            return false;
+        }
     }
 }
