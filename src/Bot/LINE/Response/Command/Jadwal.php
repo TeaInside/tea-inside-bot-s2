969f03e42bfe5b09d5d717376e42703ebc4c7d17
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-09 12:57:00 +0700

    Fix parameter

--- src/Bot/LINE/Response/Command/Jadwal.php
+++ src/Bot/LINE/Response/Command/Jadwal.php
@@ -128,7 +128,7 @@ class Jadwal extends CommandFoundation implements CommandContract
 		} else {
 			$day = $this->b['jadwalCmd'];
 			if (isset($jadwals[$day])) {
-				$jadwals = implode("\n", $jadwals[$day]);
+				$jadwals = $jadwals[$day];
 			} else {
 				return false;
 			}
